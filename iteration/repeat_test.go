7b6996557df1c5747f8b0bd9854cdd02eaad3f9e
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2022-11-04 07:15:17 +0200

    Finish maps

---
+++ iteration/repeat_test.go
@@ -0,0 +1,28 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 6)
+	expected := "aaaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+// run by: go test -bench="."
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 6)
+	}
+}
+
+func ExampleRepeat() {
+	str := Repeat("b", 5)
+	fmt.Println(str)
+	// Output: bbbbb
+}
